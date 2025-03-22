84756bbeffedb750279e966474b20f0f2ff12b7b
gustirandi17 <randigusti463@gmail.com>
2025-03-22 15:04:58 +0700

    feat: menambahkan fitur reset password dengan OTP via email

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
 //menambahkan fitur autentikasi pengguna
 //menambahkan fitur registrasi dengan validasi input
 //menambahkan middleware otorisasi untuk proteksi endpoint
+//menambahkan fitur reset password dengan OTP via email
