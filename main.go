fd7ccfdc564ee32ffde5241fe08e50a8a3d42022
gustirandi17 <randigusti463@gmail.com>
2025-03-22 15:18:09 +0700

    fix: memperbaiki perhitungan diskon yang salah pada fitur checkout

--- main.go
+++ main.go
@@ -2,3 +2,4 @@
 //memperbaiki validasi token JWT yang kadaluarsa agar pengguna diarahkan ke login ulang
 //menangani error database saat mengambil data pengguna yang tidak ditemukan
 //memperbaiki tampilan tombol submit yang tidak berubah warna saat ditekan
+//memperbaiki perhitungan diskon yang salah pada fitur checkout
