8b33cbf87a5b7f5eb6213f889fd72cf45323f51f
gustirandi17 <randigusti463@gmail.com>
2025-03-22 15:24:00 +0700

    docs: menambahkan contoh konfigurasi environment pada dokumentasi

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
 //memperbarui README.md dengan petunjuk instalasi proyek
 //menambahkan dokumentasi untuk endpoint autentikasi
 //memperbaiki typo pada CONTRIBUTING.md
+//menambahkan contoh konfigurasi environment pada dokumentasi
