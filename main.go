c6bb0b811592efd43b46e619ba7e62ca2cfe94a5
gustirandi17 <randigusti463@gmail.com>
2025-03-22 15:05:44 +0700

    feat: menambahkan endpoint GET /profile untuk menampilkan data pengguna

--- main.go
+++ main.go
@@ -2,3 +2,4 @@
 //menambahkan fitur registrasi dengan validasi input
 //menambahkan middleware otorisasi untuk proteksi endpoint
 //menambahkan fitur reset password dengan OTP via email
+//menambahkan endpoint GET /profile untuk menampilkan data pengguna
