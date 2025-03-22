cec998d40781579e7550a00c79eb7bd372a9d585
gustirandi17 <randigusti463@gmail.com>
2025-03-22 15:24:23 +0700

    docs: memperbarui CHANGELOG dengan perubahan terbaru

--- main.go
+++ main.go
@@ -2,3 +2,4 @@
 //menambahkan dokumentasi untuk endpoint autentikasi
 //memperbaiki typo pada CONTRIBUTING.md
 //menambahkan contoh konfigurasi environment pada dokumentasi
+//memperbarui CHANGELOG dengan perubahan terbaru
